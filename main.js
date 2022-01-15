44613b96bb327d6658231e12450e9cd9a083a10d
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2022-01-15 18:25:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 song1="";
 song2="";
+leftWristX=0;
+leftWristY=0;
+rightWristX=0;
+rightWristY=0;
 
 function preload(){
     song1=loadSound("music.mp3");
@@ -11,6 +15,24 @@ function setup(){
     canvas.position(450,225);
     video=createCapture(VIDEO);
     video.hide();
+    poseNet=ml5.poseNet(video,modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized");
+}
+
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist X: "+leftWristX+" left wrist Y: "+leftWristY);
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right wrist X: "+rightWristX+" right wrist Y: "+rightWristY);
+    }
 }
 
 function draw(){
