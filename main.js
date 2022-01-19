d589239ea9ea5536e6fbb1466ad4783a7b4e1e64
ananya-naveen <84455616+ananya-naveen@users.noreply.github.com>
2022-01-19 19:04:28 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ leftWristY=0;
 rightWristX=0;
 rightWristY=0;
 leftScore=0;
+rightScore=0;
 song1Status="";
 song2Status="";
 
@@ -30,7 +31,9 @@ function gotPoses(results){
     if(results.length>0){
         console.log(results);
         leftScore=results[0].pose.keypoints[9].score;
+        rightScore=results[0].pose.keypoints[10].score;
         console.log("Left Wrist Score: "+leftScore);
+        console.log("Right Wrist Score: "+rightScore);
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
         console.log("left wrist X: "+leftWristX+" left wrist Y: "+leftWristY);
@@ -52,6 +55,14 @@ function draw(){
         if(song1Status==false){
             song1.play();
             document.getElementById("name").innerHTML="Playing- Harry Potter Theme";
+        }
+    }
+    if(rightScore>0.2){
+        circle(rightWristX,rightWristY,20);
+        song1.stop();
+        if(song2Status==false){
+            song2.play();
+            document.getElementById("name").innerHTML="Playing- Peter Pan Song";
         }
     }
 }
